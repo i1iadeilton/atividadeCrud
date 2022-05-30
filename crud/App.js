2608215b1c796d3a03f13d31c40388b6691d4f15
i1iadeilton <i1iadeilton@gmail.com>
2022-05-30 10:38:29 -0300

    Atualização 2

--- crud/App.js
+++ crud/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 
 import AppTab from './AppTab';
 
+
 export default function App() {
   return (
     <>
       <AppTab />
       <StatusBar style="light" />
+      
     </>
+    
   );
-};
+}
